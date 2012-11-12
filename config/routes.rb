6e7cb93a296bdf239cfab3aa78f231270734ddf9
vered bauer <veredbr2000@yahoo.com>
2012-11-12 11:23:03 -0600

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 DoApp::Application.routes.draw do
+  resources :tasks
+
+  resources :users
+  root :to => 'tasks#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
